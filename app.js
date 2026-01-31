647d5a400ac272a086745a90081596725de86ada
ArthurPeyre <peyre.arthur@outlook.fr>
2026-01-31 03:20:31 +0100

    Refactor layout and improve styling for better user experience

--- app.js
+++ app.js
@@ -36,120 +36,119 @@ class Draw {
 // GLOBAL VARIABLES
 const draws = [];
 let currentDraw = new Draw;
-let gain = 0;
-let spent = 0;
-let coin = 50;
-let selfDraw = true;
+const gains = [
+    149651,
+    987,
+    336.9,
+    56.5
+]
 
-let gridContainer;
-let drawContainer;
-let luckyGridContainer;
-let luckyNumberContainer;
-let validateDrawBtn;
-
-
-const profit = () => {
-    return gain - spent;
-}
+let numbers;
+let luckyNumber;
+let randomDrawBtn;
+let submitContainer;
+let submitBtn;
+let gain;
 
 
+// REFRESH FUNCTIONS
 const refreshGrid = () => {
-    gridContainer.innerHTML = '';
-    luckyGridContainer.innerHTML = '';
-    drawContainer.innerHTML = '';
-    luckyNumberContainer.innerHTML = '';
-
-    if (!selfDraw) return;
-    
-    // Main grid
-    for (let i=0; i<5; i++) {
-
-        let html = '';
-
-        html += `<div class="row">`;
-
-        for (let j=1; j<=10; j++) {
-            let num = i*10 + j;
-            if (num !== 50) html += `<button class="number ${currentDraw.numbers.find(number => number == num) ? 'selected' : ''}" onclick="toggleNumber(${num})">${num}</button>`;
-        }
-
-        html += `</div>`;
-
-        gridContainer.innerHTML += html;
+    numbers.innerHTML = '';
+    for (let i=1; i<=49; i++) {
+        numbers.innerHTML += `<div class="number ${currentDraw.numbers.find(num => num == i) ? 'selected' : ''}" onclick="toggleNumber(${i})">${i}</div>`;
     }
 
-    // Lucky grid
-    let html = `<div class="row">`;
-    for (let j=1; j<=10; j++) {
-        html += `<button class="number lucky ${currentDraw.luckyNumber == j ? 'selected' : ''}" onclick="toggleLuckyNumber(${j})">${j}</button>`;
-    }
-    html += `</div>`;
-    luckyGridContainer.innerHTML = html;
-
-    currentDraw.numbers.forEach(num => {
-        drawContainer.innerHTML += `<button class="number">${num}</button>`;
-    });
-    luckyNumberContainer.innerHTML = currentDraw.luckyNumber ? `<button class="number lucky selected">${currentDraw.luckyNumber}</button>` : '';
-
-    if (currentDraw.isValide()) {
-        validateDrawBtn.classList.remove('disabled');
-    } else {
-        validateDrawBtn.classList.add('disabled');
+    luckyNumber.innerHTML = '';
+    for (let i=1; i<=10; i++) {
+        luckyNumber.innerHTML += `<div class="number ${currentDraw.luckyNumber == i ? 'selected' : ''}" onclick="toggleLuckyNumber(${i})">${i}</div>`;
     }
 }
 
+const refreshSubmitBtn = () => {
+    if (currentDraw.isValide()) {
+        submitContainer.classList.add('validated');
+    } else {
+        submitContainer.classList.remove('validated');
+    }
+}
 
-// CLICK ON NUMBER
+const refreshUI = () => {
+    refreshGrid();
+    refreshSubmitBtn();
+}
+
+
 const toggleNumber = (num) => {
-    if (currentDraw.numbers.find(number => num == number)) {
+    if (currentDraw.numbers.find(number => number == num)) {
         currentDraw.numbers = currentDraw.numbers.filter(number => number != num);
-        refreshGrid();
+        refreshUI();
         return;
     }
 
-    if (currentDraw.numbers.length === 5) return;
+    if (currentDraw.numbers.length >= 5) return;
 
     currentDraw.numbers.push(num);
     currentDraw.numbers.sort((a, b) => a - b);
 
-    refreshGrid();
+    refreshUI();
 }
 
-
-// CLICK ON LUCKY NUMBER
 const toggleLuckyNumber = (num) => {
-    currentDraw.luckyNumber = currentDraw.luckyNumber != num ? num : null;
+    if (currentDraw.luckyNumber == num) {
+        currentDraw.luckyNumber = null;
+    } else {
+        currentDraw.luckyNumber = num;
+    }
 
-    refreshGrid();
+    refreshUI();
+}
+
+const genDraw = () => {
+    currentDraw = Draw.randomDraw();
+    refreshUI();
 }
 
 
-const addDraw = (draw) => {
-    if (!draw.isValide()) return;
+const loto = () => {
+    const draw = Draw.randomDraw();
+    let valideNumbers = currentDraw.numbers.filter(num => draw.numbers.find(number => number == num));
+    let luckyValid = currentDraw.luckyNumber == draw.luckyNumber ? true : false;
 
-    draws.push(draw);
-
-    console.log(draws);
-
-    if (draw === currentDraw) selfDraw = false;
     
-    draw.numbers = [];
-    draw.luckyNumber = null;
+    if (valideNumbers.length >= 3) {
+        console.log(`Tirage: ${draw.numbers}\nNuméros communs: ${valideNumbers.length}`);
+        if (luckyValid) {
+            gain.innerHTML = gains[3];
+        }
 
-    refreshGrid();
+        if (valideNumbers.length >=4) {
+            gain.innerHTML = gains[2];
+            if (luckyValid) {
+                gain.innerHTML = gains[1];
+            }
+        }
+
+        if (valideNumbers.length == 5) {
+            gain.innerHTML = gains[0];
+        }
+    } else {
+        gain.innerHTML = 0;
+    }
 }
 
 
 window.addEventListener('DOMContentLoaded', () => {
-    gridContainer = document.getElementById('grid');
-    drawContainer = document.getElementById('draw__container');
-    luckyGridContainer = document.getElementById('luckyGrid');
-    luckyNumberContainer = document.getElementById('luckyNumber');
-
-    validateDrawBtn = document.getElementById('validateDrawBtn');
-    validateDrawBtn.addEventListener('click', () => {
-        addDraw(currentDraw);
+    numbers = document.getElementById('numbers');
+    luckyNumber = document.getElementById('luckyNumber');
+    randomDrawBtn = document.getElementById('randomBtn');
+    randomDrawBtn.addEventListener('click', genDraw);
+    submitContainer = document.getElementById('submitBtn__container');
+    submitBtn = document.getElementById('submitBtn');
+    submitBtn.addEventListener('click', () => {
+        if (!currentDraw.isValide()) return;
+        loto();
     });
+    gain = document.getElementById('gain');
 
-    refreshGrid();
+    refreshUI();
 });
